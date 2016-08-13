e71e14840b9ce340ac9ec645a660afb03e2ba6e7
yuyuhang <352091626@qq.com>
2016-08-13 23:56:10 +0800

    后台缓存任务结构

--- app/src/main/java/com/justwayward/reader/service/DownloadBookService.java
+++ app/src/main/java/com/justwayward/reader/service/DownloadBookService.java
@@ -5,11 +5,19 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
+import com.justwayward.reader.bean.ChapterRead;
+
+import java.util.List;
+
 /**
  * @author yuyh.
  * @date 16/8/13.
  */
 public class DownloadBookService extends Service {
+
+    public static final String INTENT_CHAPTER_LIST = "chapter_list";
+    public static final String INTENT_START_CHAPTER = "start_chapter";
+    public static final String INTENT_END_CHAPTER = "end_chapter";
 
     @Nullable
     @Override
@@ -19,6 +27,12 @@ public class DownloadBookService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
+        List<ChapterRead.Chapter> list = (List<ChapterRead.Chapter>) intent.getSerializableExtra(INTENT_CHAPTER_LIST);
+        int start = intent.getIntExtra(INTENT_START_CHAPTER, 0);
+        int end = intent.getIntExtra(INTENT_END_CHAPTER, 0);
+
+        // 开启下载任务
         return super.onStartCommand(intent, flags, startId);
     }
 
