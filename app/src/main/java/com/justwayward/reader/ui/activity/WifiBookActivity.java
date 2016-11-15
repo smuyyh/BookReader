748f9552158103157cea2e495f9d2bde44a48001
smuyyh <352091626@qq.com>
2016-11-15 17:56:00 +0800

    优化Wifi传书功能

--- app/src/main/java/com/justwayward/reader/ui/activity/WifiBookActivity.java
+++ app/src/main/java/com/justwayward/reader/ui/activity/WifiBookActivity.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016 JustWayward Team
- * <p>
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.TextView;
 
 import com.justwayward.reader.R;
@@ -30,6 +32,7 @@ import com.justwayward.reader.wifitransfer.Defaults;
 import com.justwayward.reader.wifitransfer.ServerRunner;
 
 import butterknife.Bind;
+import butterknife.OnClick;
 
 /**
  * Created by xiaoshu on 2016/10/9.
@@ -44,6 +47,9 @@ public class WifiBookActivity extends BaseActivity {
     TextView mTvWifiName;
     @Bind(R.id.mTvWifiIp)
     TextView mTvWifiIp;
+
+    @Bind(R.id.tvRetry)
+    TextView tvRetry;
 
     @Override
     public int getLayoutId() {
@@ -66,11 +72,23 @@ public class WifiBookActivity extends BaseActivity {
 
     @Override
     public void initDatas() {
-        mTvWifiName.setText(NetworkUtils.getConnectWifiSsid(mContext).replace("\"", ""));
-        mTvWifiIp.setText("http://" + NetworkUtils.getConnectWifiIp(mContext) + ":" + Defaults.getPort());
+        String wifiname = NetworkUtils.getConnectWifiSsid(mContext);
+        if (!TextUtils.isEmpty(wifiname)) {
+            mTvWifiName.setText(wifiname.replace("\"", ""));
+        } else {
+            mTvWifiName.setText("Unknow");
+        }
 
-        // 启动wifi传书服务器
-        ServerRunner.startServer();
+        String wifiIp = NetworkUtils.getConnectWifiIp(mContext);
+        if (!TextUtils.isEmpty(wifiIp)) {
+            tvRetry.setVisibility(View.GONE);
+            mTvWifiIp.setText("http://" + NetworkUtils.getConnectWifiIp(mContext) + ":" + Defaults.getPort());
+            // 启动wifi传书服务器
+            ServerRunner.startServer();
+        } else {
+            mTvWifiIp.setText("请开启Wifi并重试");
+            tvRetry.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
@@ -78,23 +96,32 @@ public class WifiBookActivity extends BaseActivity {
 
     }
 
+    @OnClick(R.id.tvRetry)
+    public void retry() {
+        initDatas();
+    }
+
     @Override
     public void onBackPressed() {
-        new AlertDialog.Builder(this)
-                .setTitle("提示")
-                .setMessage("确定要关闭？Wifi传书将会中断！")
-                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        finish();
-                    }
-                }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        }).create().show();
+        if (ServerRunner.serverIsRunning) {
+            new AlertDialog.Builder(this)
+                    .setTitle("提示")
+                    .setMessage("确定要关闭？Wifi传书将会中断！")
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            finish();
+                        }
+                    }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).create().show();
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
--- app/src/main/java/com/justwayward/reader/utils/NetworkUtils.java
+++ app/src/main/java/com/justwayward/reader/utils/NetworkUtils.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016 JustWayward Team
- * <p>
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -120,7 +120,8 @@ public class NetworkUtils {
     public static boolean isWifiConnected(Context context) {
         ConnectivityManager cm = (ConnectivityManager) context
                 .getSystemService(Context.CONNECTIVITY_SERVICE);
-        return cm != null && cm.getActiveNetworkInfo().getType() == ConnectivityManager.TYPE_WIFI;
+        return cm != null && cm.getActiveNetworkInfo() != null
+                && cm.getActiveNetworkInfo().getType() == ConnectivityManager.TYPE_WIFI;
     }
 
     /**
@@ -282,7 +283,7 @@ public class NetworkUtils {
             WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
             WifiInfo wifiInfo = wifiManager.getConnectionInfo();
             int ipAddress = wifiInfo.getIpAddress();
-            if(ipAddress==0){
+            if (ipAddress == 0) {
                 return null;
             }
             return ((ipAddress & 0xff) + "." + (ipAddress >> 8 & 0xff) + "."
